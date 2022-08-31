cb0542cc88f00f8e3b41db565099b3dabaa950f7
roman-thapa <romanthapa7795@gmail.com>
2022-08-31 10:46:39 +0530

    Define input of user and after selection from user
    Printed what user selected inside prompt
    Depending upon user input told what they have selected

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 console.log("Welcome to Rock Paper Scissor Game");
-let inputFromUser = prompt("Please select", "1=rock, 2=paper, 3=scissor");
+let inputFromUser;
+userInput();
+function userInput() {
+    inputFromUser = prompt("Please select", "1=rock, 2=paper, 3=scissor");
+    switch (inputFromUser) {
+        case "1":
+            console.log("You have selected Rock");
+            break;
+        
+        case "2":
+            console.log("You have selected Paper");
+            break;
+        
+        case "3":
+            console.log("You have selected Scissor");
+            break;
+    
+        default:
+            console.log("Invalid input!\n Try again");
+            userInput();
+    }
+    return inputFromUser;
+}
 console.log(inputFromUser);
+/*switch (inputFromUser) {
+    case "1":
+        console.log("You have selected Rock");
+        break;
+    
+    case "2":
+        console.log("You have selected Paper");
+        break;
+    
+    case "3":
+        console.log("You have selected Scissor");
+        break;
+
+    default:
+        console.log("Invalid input!\n Try again");
+}*/
