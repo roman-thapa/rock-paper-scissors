fffc8b44ff51e178878f540377b368354ed96095
roman-thapa <romanthapa7795@gmail.com>
2022-09-02 10:37:10 +0530

    Make coditions to win or lose
    Made a function which will check condition of winning and losing
    And tell user if they win or loose
    If its a draw it will start game from beginning

--- script.js
+++ script.js
@@ -18,9 +18,10 @@ function userInput() {
             break;
     
         default:
-            console.log("Invalid input!\n Try again");
+            alert("Invalid input!\n Try again");
             userInput();
     }
+    inputFromUser = parseInt(inputFromUser);
     return inputFromUser;
 }
 
@@ -29,7 +30,6 @@ function userInput() {
 getComputerChoice();
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random()*(4-1)+1);
-    console.log(computerChoice)
     switch (computerChoice) {
         case 1:
             console.log("PC have selected Rock");
@@ -44,4 +44,33 @@ function getComputerChoice() {
             break;
     }
     return computerChoice;
+}
+
+
+getResult(inputFromUser, computerChoice);
+
+function getResult(userChoice, pcChoice) {
+
+    if(userChoice==1 && pcChoice==3) {
+        console.log("You win!");
+    }
+
+    else if(userChoice==3 && pcChoice==1) {
+        console.log("You lose!");
+    }
+
+    else if (userChoice>pcChoice){
+        console.log("You win!");
+    }
+
+    else if (userChoice==pcChoice) {
+        console.log("It's a draw!\nTry again");
+        userInput();
+        getComputerChoice();
+        getResult(inputFromUser, computerChoice);
+    }
+
+    else{
+        console.log("You lose!");
+    }
 }
